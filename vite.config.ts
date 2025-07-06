fbf2dff7742f68dc321c09d77f624e135da0d040
Soczó <lonsdale201@hotmail.com>
2025-07-06 14:55:28 +0200

    Configure Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: process.env.NODE_ENV === 'production' ? '/shadow-games-p2p/' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['svelte'],
+          peerjs: ['peerjs'],
+          granim: ['granim']
+        }
+      }
+    }
+  }
 })
