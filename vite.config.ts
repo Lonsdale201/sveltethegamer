6db57d48d910305f6740f9bb0c12fc3bd21c478a
Soczó <lonsdale201@hotmail.com>
2025-07-06 15:15:05 +0200

    Update vite.config.ts for relative base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: process.env.NODE_ENV === 'production' ? '/shadow-games-p2p/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/sveltethegamer/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
